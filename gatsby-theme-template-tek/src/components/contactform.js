9cb36a66c21f3aa0050141a30b8aafee16d7d45d
Shane Fisher <shane.fisher@warrendouglas.com>
2019-11-20 15:38:18 -0600

    adjust form for netlify integration

--- gatsby-theme-template-tek/src/components/contactform.js
+++ gatsby-theme-template-tek/src/components/contactform.js
@@ -7,11 +7,20 @@ import Img from 'gatsby-image'
 const contactform = () => {
   return (
     <div>
-      <Styled.h1 sx={{ color: `secondary`, marginBottom: `0`, fontSize: ['3', '3', '3', '6', '6'] }}>CONTACT <span sx={{ color: `primary` }}>TEK</span> INDUSTRIES</Styled.h1>
+      <Styled.h1
+        sx={{
+          color: `secondary`,
+          marginBottom: `0`,
+          fontSize: ['3', '3', '3', '6', '6'],
+        }}
+      >
+        CONTACT <span sx={{ color: `primary` }}>TEK</span> INDUSTRIES
+      </Styled.h1>
       <form
         name="contact"
-        method="POST"
+        method="post"
         data-netlify="true"
+        data-netlify-honeypot="bot-field"
         css={css`
           input {
             width: 100%;
@@ -34,29 +43,49 @@ const contactform = () => {
             font-family: inherit;
             font-size: 16px;
           }
-          button{
+          button {
             display: block;
-            border: 2px solid #D23A00;
+            border: 2px solid #d23a00;
             padding: 15px 30px;
-            color: #D23A00;
+            color: #d23a00;
             cursor: pointer;
             background: #fff;
           }
         `}
       >
+        <input type="hidden" name="form-name" value="contact" />
         <input type="text" placeholder="Name" name="name" />
         <input type="email" placeholder="Email" name="email" />
         <input type="company" placeholder="Company" name="company" />
-        <textarea placeholder="Let us know about your roofing needs" name="message"></textarea>
+        <textarea
+          placeholder="Let us know about your roofing needs"
+          name="message"
+        ></textarea>
         <button type="submit">Send message</button>
       </form>
       <address sx={{ color: `gray`, fontStyle: `normal`, margin: `5% 0 0 0` }}>
-        Corp Office <span sx={{ color: `primary`, fontWeight: `bold` }}>877-729-5229</span><br />
-        Monday – <span sx={{ color: `primary`, fontWeight: `bold` }}>Friday 8a.m. - 5p.m. pst</span><br />
-        Manufactured by TEK Industries, LLC 11801 Pierce Street, Suite 200,<br />
+        Corp Office{' '}
+        <span sx={{ color: `primary`, fontWeight: `bold` }}>877-729-5229</span>
+        <br />
+        Monday –{' '}
+        <span sx={{ color: `primary`, fontWeight: `bold` }}>
+          Friday 8a.m. - 5p.m. pst
+        </span>
+        <br />
+        Manufactured by TEK Industries, LLC 11801 Pierce Street, Suite 200,
+        <br />
         Riverside, California, 92505
       </address>
-      <p sx={{ color: `gray`, display: ['none', 'none', 'block', 'block', 'block'] }}>At TEK® Industries, our mission is two-fold: to expand our roofing family and to renovate the entire roofing market by giving more to homeowners.</p>
+      <p
+        sx={{
+          color: `gray`,
+          display: ['none', 'none', 'block', 'block', 'block'],
+        }}
+      >
+        At TEK® Industries, our mission is two-fold: to expand our roofing
+        family and to renovate the entire roofing market by giving more to
+        homeowners.
+      </p>
     </div>
   )
 }
