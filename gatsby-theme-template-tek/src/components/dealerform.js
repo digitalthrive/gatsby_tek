417350cad401f2e310a756bf784f2017b0555380
Shane Fisher <shane.fisher@warrendouglas.com>
2019-12-13 11:45:42 -0600

    adjusts dealer form fields

--- gatsby-theme-template-tek/src/components/dealerform.js
+++ gatsby-theme-template-tek/src/components/dealerform.js
@@ -3,6 +3,65 @@ import { Styled, jsx } from 'theme-ui'
 import { css } from '@emotion/core'
 
 const dealerform = () => {
+  let states = [
+    ['Alabama', 'AL'],
+    ['Alaska', 'AK'],
+    ['Arizona', 'AZ'],
+    ['Arkansas', 'AR'],
+    ['California', 'CA'],
+    ['Colorado', 'CO'],
+    ['Connecticut', 'CT'],
+    ['Delaware', 'DE'],
+    ['District of Columbia', 'DC'],
+    ['Florida', 'FL'],
+    ['Georgia', 'GA'],
+    ['Hawaii', 'HI'],
+    ['Idaho', 'ID'],
+    ['Illinois', 'IL'],
+    ['Indiana', 'IN'],
+    ['Iowa', 'IA'],
+    ['Kansas', 'KS'],
+    ['Kentucky', 'KY'],
+    ['Louisiana', 'LA'],
+    ['Maine', 'ME'],
+    ['Maryland', 'MD'],
+    ['Massachusetts', 'MA'],
+    ['Michigan', 'MI'],
+    ['Minnesota', 'MN'],
+    ['Mississippi', 'MS'],
+    ['Missouri', 'MO'],
+    ['Montana', 'MT'],
+    ['Nebraska', 'NE'],
+    ['Nevada', 'NV'],
+    ['New Hampshire', 'NH'],
+    ['New Jersey', 'NJ'],
+    ['New Mexico', 'NM'],
+    ['New York', 'NY'],
+    ['North Carolina', 'NC'],
+    ['North Dakota', 'ND'],
+    ['Ohio', 'OH'],
+    ['Oklahoma', 'OK'],
+    ['Oregon', 'OR'],
+    ['Pennsylvania', 'PA'],
+    ['Puerto Rico', 'PR'],
+    ['Rhode Island', 'RI'],
+    ['South Carolina', 'SC'],
+    ['South Dakota', 'SD'],
+    ['Tennessee', 'TN'],
+    ['Texas', 'TX'],
+    ['Utah', 'UT'],
+    ['Vermont', 'VT'],
+    ['Virginia', 'VA'],
+    ['Washington', 'WA'],
+    ['Washington D.C.', 'DC'],
+    ['West Virginia', 'WV'],
+    ['Wisconsin', 'WI'],
+    ['Wyoming', 'WY'],
+    ['Armed Forces America', 'Armed Forces America'],
+    ['Armed Forces Europe', 'Armed Forces Europe'],
+    ['Armed Forces Pacific', 'Armed Forces Pacific'],
+  ]
+
   return (
     <div>
       <Styled.h1
@@ -50,6 +109,9 @@ const dealerform = () => {
             font-size: 18px;
             text-indent: 18px;
           }
+          .state_location {
+            margin-bottom: 30px;
+          }
           input[type='radio'] {
             width: auto;
             height: auto;
@@ -83,6 +145,12 @@ const dealerform = () => {
       >
         <input type="hidden" name="bot-field" />
         <input type="hidden" name="form-name" value="dealer" />
+        <p>What state are you in?</p>
+        <select name="state_location" className="state_location">
+          {states.map(state => {
+            return <option value={state[1]}>{state[0]}</option>
+          })}
+        </select>
         <label htmlFor="company_name">Company Name</label>
         <input type="text" placeholder="Company Name" name="company_name" />
         <label htmlFor="company_address">Company Address</label>
@@ -99,12 +167,7 @@ const dealerform = () => {
         />
         <label htmlFor="contact_name">Contact Name</label>
         <input type="text" placeholder="Contact Name" name="contact_name" />
-        <label htmlFor="role[]">Your Role: </label>
-        <select name="role">
-          <option value="none">Choose Role</option>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select>
+        <input type="text" placeholder="Position" name="position" />
         <label htmlFor="email">Email</label>
         <input type="email" placeholder="Email" name="email" />
         <label htmlFor="phone">Phone (optional)</label>
@@ -117,6 +180,61 @@ const dealerform = () => {
         <input type="radio" name="years_in_business" value="11-15" /> 11-15
         <br />
         <input type="radio" name="years_in_business" value="15+" /> 15+
+        <br />
+        <br />
+        <p>Gross Annual Revenue</p>
+        <input type="radio" name="gross_annual_revenue" value="500k-1m" />{' '}
+        $500,000 - $1,000,000
+        <br />
+        <input type="radio" name="gross_annual_revenue" value="1m-3m" />{' '}
+        $1,000,000 - $3,000,000
+        <br />
+        <input type="radio" name="gross_annual_revenue" value="3m-6m" />{' '}
+        $3,000,000 - $6,000,000
+        <br />
+        <input type="radio" name="gross_annual_revenue" value="6m+" />{' '}
+        $6,000,000+
+        <br />
+        <br />
+        <p>Number of Sales Staff</p>
+        <input type="radio" name="sales_staff" value="1-5" /> 1-5
+        <br />
+        <input type="radio" name="sales_staff" value="6-10" /> 6-10
+        <br />
+        <input type="radio" name="sales_staff" value="11-15" /> 11-15
+        <br />
+        <input type="radio" name="sales_staff" value="15+" /> 15+
+        <br />
+        <br />
+        <p>Annual Square of Metal Installed</p>
+        <input type="radio" name="installed_sq_ft" value="500-1000" /> 500 -
+        1,000 sq ft
+        <br />
+        <input type="radio" name="installed_sq_ft" value="1000-3000" /> 1,000 -
+        3,000 sq ft
+        <br />
+        <input type="radio" name="installed_sq_ft" value="3000-6000" /> 3,000 -
+        6,000 sq ft
+        <br />
+        <input type="radio" name="installed_sq_ft" value="6000+" /> 6,000+
+        <br />
+        <br />
+        <p>Do you have a warehouse with a loading dock?</p>
+        <select name="loading_dock">
+          <option value="Yes">Yes</option>
+          <option value="No">No</option>
+        </select>
+        <p>Square Feet of Warehouse Space</p>
+        <input type="radio" name="warehouse_sq_ft" value="500-1000" /> 500 -
+        1,000 sq ft
+        <br />
+        <input type="radio" name="warehouse_sq_ft" value="1000-3000" /> 1,000 -
+        3,000 sq ft
+        <br />
+        <input type="radio" name="warehouse_sq_ft" value="3000-6000" /> 3,000 -
+        6,000 sq ft
+        <br />
+        <input type="radio" name="warehouse_sq_ft" value="6000+" /> 6,000+ sq ft
         <br />
         <br />
         <label htmlFor="how_did_you_hear_about_TEK">
--- gatsby-theme-template-tek/src/components/layout.js
+++ gatsby-theme-template-tek/src/components/layout.js
@@ -48,9 +48,10 @@ const DealerHidden = () => {
       >
         <input type="hidden" name="bot-field" />
         <input type="hidden" name="form-name" value="dealer" />
-        <input type="hidden" name="bot-field" />
-        <input type="hidden" name="form-name" value="dealer" />
         <input type="text" placeholder="Company Name" name="company_name" />
+        <select name="state_location" className="state_location">
+          <option value="AL">Alabama</option>
+        </select>
         <input
           type="text"
           placeholder="Company Address"
@@ -62,17 +63,43 @@ const DealerHidden = () => {
           name="company_website"
         />
         <input type="text" placeholder="Contact Name" name="contact_name" />
-        <select name="role">
-          <option value="none">Choose Role</option>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select>
+        <input type="text" placeholder="Position" name="position" />
         <input type="email" placeholder="Email" name="email" />
         <input type="phone" placeholder="Phone (optional)" name="phone" />
         <input type="radio" name="years_in_business" value="1-5" /> 1-5
         <input type="radio" name="years_in_business" value="6-10" /> 6-10
         <input type="radio" name="years_in_business" value="11-15" /> 11-15
         <input type="radio" name="years_in_business" value="15+" /> 15+
+        <input type="radio" name="gross_annual_revenue" value="500k-1m" />
+        $500,000 - $1,000,000
+        <input type="radio" name="gross_annual_revenue" value="1m-3m" />
+        $1,000,000 - $3,000,000
+        <input type="radio" name="gross_annual_revenue" value="3m-6m" />
+        $3,000,000 - $6,000,000
+        <input type="radio" name="gross_annual_revenue" value="6m+" />
+        $6,000,000+
+        <input type="radio" name="sales_staff" value="1-5" /> 1-5
+        <input type="radio" name="sales_staff" value="6-10" /> 6-10
+        <input type="radio" name="sales_staff" value="11-15" /> 11-15
+        <input type="radio" name="sales_staff" value="15+" /> 15+
+        <input type="radio" name="installed_sq_ft" value="500-1000" /> 500 -
+        1,000 sq ft
+        <input type="radio" name="installed_sq_ft" value="1000-3000" /> 1,000 -
+        3,000 sq ft
+        <input type="radio" name="installed_sq_ft" value="3000-6000" /> 3,000 -
+        6,000 sq ft
+        <input type="radio" name="installed_sq_ft" value="6000+" /> 6,000+
+        <select name="loading_dock">
+          <option value="Yes">Yes</option>
+          <option value="No">No</option>
+        </select>
+        <input type="radio" name="warehouse_sq_ft" value="500-1000" /> 500 -
+        1,000 sq ft
+        <input type="radio" name="warehouse_sq_ft" value="1000-3000" /> 1,000 -
+        3,000 sq ft
+        <input type="radio" name="warehouse_sq_ft" value="3000-6000" /> 3,000 -
+        6,000 sq ft
+        <input type="radio" name="warehouse_sq_ft" value="6000+" /> 6,000+ sq ft
         <textarea name="how_did_you_hear_about_TEK"></textarea>
         <textarea name="describe_why_you_are_for_for_a_dealer"></textarea>
         <p>
