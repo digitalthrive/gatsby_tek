cf637bb7b38964daf633b7243476c71ecbdaf698
Shane Fisher <shane.fisher@warrendouglas.com>
2019-12-17 11:26:58 -0600

    fixes minheight on products chooser

--- gatsby-theme-template-tek/src/pages/products.js
+++ gatsby-theme-template-tek/src/pages/products.js
@@ -188,7 +188,7 @@ const products = cold(({ data, location }) => {
             </div>
           </div>
         ) : (
-          <div sx={{ height: `400px` }}>
+          <div sx={{ minHeight: [null, null, null, `400px`, `400px`] }}>
             <div
               sx={{
                 display: `flex`,
