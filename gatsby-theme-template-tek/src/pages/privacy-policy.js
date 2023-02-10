cc1bae8973644cf6d08719f502d0c967d2d22883
Manuel Novoa <novoamanuel@ymail.com>
2023-02-10 10:21:54 -0600

    Privacy Policy page added.

---
+++ gatsby-theme-template-tek/src/pages/privacy-policy.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import Layout from '../components/layout';
+import Section from '../components/section'
+import { Helmet } from 'react-helmet'
+
+
+import "../components/privacy-policy.css";
+
+const privacyPolicy = () =>{
+    return(
+        <Layout>
+
+            <Helmet>
+                <title>
+                    TEK® Industries | The Best Roof For Severe Weather | View Our Metal
+                    Roofs
+                </title>
+                <meta
+                    name="description"
+                    content="With TEK Industries, you know you are getting the most durable metal roof for severe weather. View our metal shakes and their many benefits!"
+                />
+                <meta
+                    name="keywords"
+                    content="benefits of metal roof, roof for severe weather, hurricane roof, durable roof, durable metal roof, hurricane metal roof, roof for bad weather, metal roof for bad weather"
+                />
+            </Helmet>
+
+        <Section>
+            <div className='container-privacy'>
+            <p className="p1"><span className="s1"><strong>TEK Industries, LLC Online Privacy Policy</strong></span></p>
+            <p className="p2"><br/></p>
+            <p className="p3"><strong>Last Updated:</strong><span className="Apple-converted-space">&nbsp;&nbsp;</span>February 4, 2023</p>
+            <p className="p2"><br/></p>
+            <p className="p3">This Online Privacy Policy (&ldquo;<strong>Policy</strong>&rdquo;) describes how TEK Industries, LLC (&ldquo;<strong>we</strong>&rdquo;, &ldquo;<strong>us</strong>&rdquo;, or &ldquo;<strong>our</strong>&rdquo;) collects, uses, and discloses the personal information that we obtain through <a href="https://www.tekroofingsystems.com/"><span className="s2">tekroofingsystems.com</span></a> (&ldquo;<strong>Website</strong>&rdquo;). This Policy does not apply to other sites or online services that we maintain, and do not link to this Policy.</p>
+            <p className="p4"><br/></p>
+            <p className="p5"><span className="s1"><strong>INFORMATION WE COLLECT<span className="Apple-converted-space">&nbsp;</span></strong></span></p>
+            <p className="p4"><br/></p>
+            <p className="p6">When you use our Website you may choose to submit to us certain personal information such as your name, address, email address, telephone number, and <span className="s3">professional details (such as organization name and title).</span></p>
+            <p className="p4"><br/></p>
+            <p className="p6">In addition to information you submit to us through the Website, we and our vendors also obtain information about visitors to our Website using automated tracking technologies, like cookies. Tracking technologies may collect information such as IP address, device ID, pages visited, the content you interact with, and the time of the visit. Generally, these tracking technologies are used to run our Website, optimize functionality, personalize content for you, collect analytics about visitors, and to facilitate marketing and targeted advertising.<span className="Apple-converted-space">&nbsp;</span></p>
+            <p className="p4"><br/></p>
+            <p className="p7">For example, we may use Google services such as Google APIs or Google Analytics, including features such as audience targeting, behavioral reporting, demographics and interests reporting, and user segment analysis. For more information on how Google Analytics uses data collected through the Website, visit: <a href="http://www.google.com/policies/privacy/partners/"><span className="s2">www.google.com/policies/privacy/partners/</span></a>. To opt out of Google Analytics cookies, visit: <a href="http://www.google.com/settings/ads"><span className="s2">http://www.google.com/settings/ads</span></a> and <a href="https://tools.google.com/dlpage/gaoptout/"><span className="s2">https://tools.google.com/dlpage/gaoptout/</span></a>.<span className="Apple-converted-space">&nbsp;</span></p>
+            <p className="p6">Do Not Track (&ldquo;DNT&rdquo;) is a privacy preference signal that may allow you to opt out of online tracking. Currently, our Website does not respond to DNT signals. You may be able to adjust the settings on your browser to limit cookies and other trackers. If you do limit cookies, you may not be able to access or use certain portions of the Website and/or certain offerings on the Website may not function as intended. <span className="Apple-converted-space">&nbsp;</span></p>
+            <p className="p4"><br/></p>
+            <p className="p5">Please note that we or external parties may combine certain tracking technology information with other information we obtain about you, and may track your online activities over time and across different devices and online services.</p>
+            <p className="p4"><br/></p>
+            <p className="p7"><span className="s1"><strong>USE AND DISCLOSURE OF INFORMATION WE COLLECT</strong></span></p>
+            <p className="p8">We collect, use, disclose, or otherwise process your personal information for various purposes, including the following:</p>
+            <ul className="ul1">
+                <li className="li9">To tailor your experience on our Website, protect the security and reliability of our Website, provide services to you, address your inquiries, and provide customer service</li>
+                <li className="li9">To provide you with information about us, our products and services, or other products and services in which we believe may be of interest to you, and to provide you with content according to these preferences including through advertising</li>
+                <li className="li9">To meet our legal obligations, work with law enforcement, to comply with laws, regulations, court orders, to assist in investigations to prevent fraud or other harms, to enforce our rights or agreements with other parties</li>
+                <li className="li9">For other purposes, as described at the time of collection of your personal information</li>
+            </ul>
+            <p className="p10"><br/></p>
+            <p className="p8">We may disclose personal information we obtain through the Website to our subsidiaries, affiliates, partners, and agents, as well as to our vendors who perform services on our behalf.<span className="Apple-converted-space">&nbsp;&nbsp;</span>In addition, we may disclose your personal information to the certain external parties, including:<strong><span className="Apple-converted-space">&nbsp;</span></strong></p>
+            <ul className="ul1">
+                <li className="li5"><span className="s5"><strong>Business Transactions.&nbsp;</strong>We may disclose your personal information in the event we sell or transfer all or a portion of our business assets (e.g., further to a merger, reorganization, liquidation, bankruptcy, or any other business transaction), including negotiations of such transactions.<span className="Apple-converted-space">&nbsp;</span></span></li>
+                <li className="li5"><strong>Legal and Safety Purposes.&nbsp;</strong>We may disclose your personal information as required or appropriate for legal compliance purposes, for example, to law enforcement, government authorities, regulatory bodies, or other parties in order to comply with laws, regulations, court orders, or other legal obligations or to assist in investigations to prevent fraud or other harms, to enforce our terms, this Policy, or agreements with third parties.</li>
+            </ul>
+            <p className="p2"><br/></p>
+            <p className="p3"><span className="s1"><strong>SOCIAL MEDIA AND OTHER ONLINE SERVICES<span className="Apple-converted-space">&nbsp;</span></strong></span></p>
+            <p className="p11"><br/></p>
+            <p className="p5">Our Website may provide links or otherwise facilitate access to other websites or online services, including social media services, for your convenience. This Policy does not apply to the practices of companies that we do not own or control. If you share your personal information with us through a social media service, such as to one of our official social media accounts, profiles, or pages, that personal information is governed by that social media service&rsquo;s privacy policy, which we do not control. The social media service may permit us to use information posted to their service for our own purposes. For more information, please review the privacy policies of the social media services you use.<span className="Apple-converted-space">&nbsp;</span></p>
+            <p className="p2"><br/></p>
+            <p className="p3"><span className="s1"><strong>YOUR CHOICES</strong></span></p>
+            <p className="p2"><br/></p>
+            <p className="p5">If you prefer to no longer receive marketing emails from us, you may opt out by using the unsubscribe link located in our emails. Please note that even if you opt out of marketing or promotional emails, we may still send you transactional or informational emails.<span className="Apple-converted-space">&nbsp;</span></p>
+            <p className="p12"><br/></p>
+            <p className="p5"><span className="s1"><strong>DATA SECURITY</strong></span></p>
+            <p className="p2"><br/></p>
+            <p className="p5">We take commercially reasonable security measures to protect the personal information we obtain through the Website from unauthorized access, loss, misuse, alteration, or destruction. Nevertheless, transmission via the Internet and online digital storage are not completely secure and we cannot guarantee the security of your information collected through the Site.</p>
+            <p className="p4"><br/></p>
+            <p className="p5"><span className="s1"><strong>CHANGES TO THIS POLICY</strong></span></p>
+            <p className="p12"><br/></p>
+            <p className="p5">From time to time, we may amend this Policy to reflect changes in our business or data collection practices. We will indicate such changes by revising the &ldquo;Last updated&rdquo; date above. <span className="Apple-converted-space">&nbsp;</span></p>
+            <p className="p4"><br/></p>
+            <p className="p5"><span className="s1"><strong>CONTACT US</strong></span></p>
+            <p className="p4"><br/></p>
+            <p className="p6">If you have questions or concerns about our Privacy Policy or data practices, you may contact us at c_corp@tekroofingsystems.com<strong><span className="Apple-converted-space">&nbsp;</span></strong></p>
+            </div>
+        </Section>
+        
+            
+        </Layout>
+    )
+}
+
+
+export default privacyPolicy;
